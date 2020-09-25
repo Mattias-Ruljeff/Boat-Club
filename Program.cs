4feece5a3bb41ae03aa67d22eaac8d41d9f6a0fd
MattRul <mr223mi@student.lnu.se>
2020-09-25 09:46:11 +0200

    initial push

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jolly_pirate_Yacht_Club
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
